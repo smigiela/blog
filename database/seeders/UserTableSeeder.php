9fcfd217f0d525a1b6da114b07cf56f8dd961c66
Daniel Śmigiela <daniel.smigiela@outlook.com>
2022-03-21 12:29:13 +0100

    Initialized commit with laravel starter kit

---
+++ database/seeders/UserTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory()->create([
+            'email' => 'admin@blog.com'
+        ]);
+        \App\Models\User::factory()->count(10)->create();
+    }
+}
